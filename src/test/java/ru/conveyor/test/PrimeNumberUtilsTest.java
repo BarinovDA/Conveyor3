e3b8f8941b87e7b092c7653102787aadf80be19a
BarinovDA <52528663+BarinovDA@users.noreply.github.com>
2019-11-08 18:03:59 +0300

    Class for testing utils (PrimeNumber)

---
+++ src/test/java/ru/conveyor/test/PrimeNumberUtilsTest.java
@@ -0,0 +1,21 @@
+package ru.conveyor.test;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import ru.conveyor.util.PrimeNumberUtils;
+
+public class PrimeNumberUtilsTest {
+
+
+
+    public void testUtils (){
+
+        // test PrimeNumbersUtils.isPrime
+        boolean resault = PrimeNumberUtils.isPrime(0);
+        Assert.assertThat(false, CoreMatchers.is(resault));
+
+        resault = PrimeNumberUtils.isPrime(-5);
+        Assert.assertThat(false, CoreMatchers.is(resault));
+
+    }
+}
